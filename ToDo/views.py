f1804cf9211762b625e4f48ac62a85e48c31cae0
Bella-aa <Nyashamarara28@gmail.com>
2022-11-18 16:27:54 +0000

    added task list and task views

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def home(request):
-    return HttpResponse("Working")
+from . models import Task 
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'task'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
