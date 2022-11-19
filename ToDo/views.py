e1b749ac918482900b7bdc725feac8c395749e46
Bella-aa <Nyashamarara28@gmail.com>
2022-11-19 00:26:55 +0000

    created task views and task update

--- ToDo/views.py
+++ ToDo/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
+from django.urls import reverse_lazy
 from . models import Task 
 
 class TaskList(ListView):
@@ -12,4 +14,15 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task')   
+
 
