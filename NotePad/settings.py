430d92810748a140d88f5c4fcd9cc790d42b650d
Bella-aa <Nyashamarara28@gmail.com>
2023-01-04 13:49:36 +0000

    added mysql database

--- NotePad/settings.py
+++ NotePad/settings.py
@@ -23,8 +23,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') 
+# SECURITY WARNING: keep the secret key used in production secret! 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,8 +88,8 @@ WSGI_APPLICATION = 'NotePad.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 
 # Password validation
